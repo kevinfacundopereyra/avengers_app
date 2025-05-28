762ad1f299b093c09394d3e0dd7a784f5ae374b2
kevinfacundopereyra <pereyrakevinfacundo@gmail.com>
2025-05-28 01:13:47 -0300

    get y post

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,52 @@
 import express from 'express'
+import prisma from './prisma/prismaclient.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('Hola desde la api'))
+
+app.get('/api/avengers', async (req, res) => {
+    try {
+      const avengers = await prisma.avengers.findMany({
+        include: {
+          habilidades: true
+        }
+      })
+      res.json(avengers)
+    } catch (error) {
+      console.error('Error al obtener los avengers:', error)
+      res.status(500).json({ error: 'Error al obtener los avengers' })
+    }
+  })
+
+  app.post('/api/avengers', async (req, res) => {
+    try {
+      const { nombre, alias, actor, habilidades } = req.body
+  
+      const nuevoAvenger = await prisma.avengers.create({
+        data: {
+          nombre,
+          alias,
+          actor,
+          habilidades: {
+            create: habilidades.map((h) => ({
+              habilidad: h.habilidad
+            }))
+          }
+        },
+        include: {
+          habilidades: true
+        }
+      })
+  
+      res.status(201).json(nuevoAvenger)
+    } catch (error) {
+      console.error('Error al crear el avenger:', error)
+      res.status(500).json({ error: 'Error al crear el avenger' })
+    }
+  })
 
 
 const PORT = 3000
